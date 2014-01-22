2159fa8eedb5e75008eb1520b3abfc190458929d
mdular <git@the-enemy.org>
2014-01-22 22:34:38 +0100

    Update about page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -6,8 +6,19 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-<p>My name is Markus. I'm currently living in Berlin, where i enjoy the parks in summer and compensate the lack of sun in winter. Feel free to <?php echo CHtml::link('drop me a line', $this->createUrl('site/contact')); ?>.</p>
+<p>
+My name is Markus. I'm currently living in Berlin, where i enjoy the parks 
+in summer and compensate the lack of sun in winter. 
+</p>
 
-<p>Apart from working in IT at Rocket Internet i like getting into little side projects, and this personal space is the latest one.</p>
+<p>
+Apart from working in IT at Rocket Internet i like getting into little side projects, 
+and this personal space will serve as a platform.
+</p>
 
-<p>At the moment i'm rather focused on the technical side of mdular.com, and then some design work will have to be done - so please forgive the lack of polishing and sparse content ;)</p>
+<p>
+At the moment i'm rather focused on the technical side of mdular.com, so please forgive 
+the lack of polishing and sparse content ;)
+</p>
+
+<p>Feel free to <?php echo CHtml::link('drop me a line', $this->createUrl('site/contact')); ?>.</p>
