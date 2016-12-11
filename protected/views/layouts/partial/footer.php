2816fb3fb07f744d6aee89cfd389a81cebd0bb23
Markus J Doetsch <git@the-enemy.org>
2016-12-11 12:58:10 +0100

    add cookie consent, move highlight.js to footer

--- protected/views/layouts/partial/footer.php
+++ protected/views/layouts/partial/footer.php
@@ -1,24 +1,25 @@
-    </div><!-- /#site -->
-  </div><!-- /#sitewrapper -->
-  <footer>
+</div><!-- /#site -->
+</div><!-- /#sitewrapper -->
+<footer>
     &copy; <?php echo date('Y'); ?> <?php echo Yii::app()->params['siteOwner']; ?> &ndash; <a href="<?php echo $this->createUrl('site/page', array('view'=>'imprint')) ?>" title="Imprint" rel="nofollow">Imprint</a>
-  </footer>
+</footer>
 
-  <?php Yii::app()->getClientScript()->registerScriptFile(Yii::app()->params['backgroundUrl'], CClientScript::POS_END, array(/*'async' => true, 'defer' => true TODO: fix jquery dep */)); ?>
+<?php Yii::app()->getClientScript()->registerScriptFile(Yii::app()->params['backgroundUrl'], CClientScript::POS_END, array(/*'async' => true, 'defer' => true TODO: fix jquery dep */)); ?>
 
-  <script>hljs.initHighlightingOnLoad();</script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 
-  <?php if(Yii::app()->user->isGuest && !empty(Yii::app()->params['GAid']) && !empty(Yii::app()->params['GAdomain'])): ?>
-  <script type="text/javascript">
+<?php if(Yii::app()->user->isGuest && !empty(Yii::app()->params['GAid']) && !empty(Yii::app()->params['GAdomain'])): ?>
+    <script type="text/javascript">
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', '<?php echo Yii::app()->params['GAid'] ?>']);
     _gaq.push(['_setDomainName', '<?php echo Yii::app()->params['GAdomain'] ?>']);
     _gaq.push(['_trackPageview']);
 
     var initGA = function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     };
 
     if (window.addEventListener) {
@@ -26,9 +27,17 @@
     } else if (window.attachEvent)  {
         window.attachEvent('onload', initGA);
     }
-  </script>
-  <?php endif; ?>
+    </script>
+<?php endif; ?>
 
-  <script>var serverTime = <?php echo ( time() ); ?>;</script>
+<!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+<script type="text/javascript">
+window.cookieconsent_options = {"message":"This website uses cookies","dismiss":"Got it!","learnMore":"More info","link":null,"theme":"light-bottom"};
+</script>
+
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+<!-- End Cookie Consent plugin -->
+
+<script>var serverTime = <?php echo time(); ?></script>
 </body>
 </html>
--- protected/views/layouts/partial/html_header.php
+++ protected/views/layouts/partial/html_header.php
@@ -1,27 +1,26 @@
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
 
-  <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-  <meta name="description" content="Notes and experiments related to web development, programming, frontend and technology in general">
-  <meta name="author" content="Markus J Doetsch">
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <meta name="description" content="Notes and experiments related to web development, programming, frontend and technology in general">
+    <meta name="author" content="Markus J Doetsch">
 
-  <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
+    <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
 
-  <?php /* <link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'> */ ?>
-  <?php /* <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" /> */ ?>
-  <?php /* <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'> */ ?>
+    <?php /* <link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'> */ ?>
+    <?php /* <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" /> */ ?>
+    <?php /* <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'> */ ?>
 
-  <!--[if lt IE 9]> <script src="/js/lib/modernizr-2.6.3.custom.min.js"></script> <![endif]-->
+    <!--[if lt IE 9]> <script src="/js/lib/modernizr-2.6.3.custom.min.js"></script> <![endif]-->
 
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/styles/default.min.css">
-  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/highlight.min.js"></script>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/styles/default.min.css">    
 </head>
 
 <body>
-  <div id="background" class="inactive"></div>
-  <div id="sitewrapper">
-    <div id="site" class="clearfix collapsible bg_main alpha80">
+    <div id="background" class="inactive"></div>
+    <div id="sitewrapper">
+        <div id="site" class="clearfix collapsible bg_main alpha80">
